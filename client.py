32832a7fce27978f5c7f2f41963ea6cd1a0001de
zhouzirui522 <152956431+zhouzirui522@users.noreply.github.com>
2025-04-24 22:51:52 +0800

    Initialize the client with server connection details and request file.

--- client.py
+++ client.py
@@ -2,3 +2,7 @@ import socket
 import sys
 
 class TupleSpaceClient:
+    def __init__(self, host, port, file_path):
+        self.host = host
+        self.port = port
+        self.file_path = file_path
